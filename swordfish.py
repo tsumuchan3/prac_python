c6ac509aa7276f4402624ca5a28414b4ec805d76
tsumuchan3 <is0602kv@ed.ritsumei.ac.jp>
2024-06-21 11:14:40 +0900

    6/21

---
+++ swordfish.py
@@ -0,0 +1,10 @@
+while True:
+          print("who are you?")
+          name=input()
+          if name != "joe":
+                  continue
+          print("Hi joe,what is your password?")
+          password=input()
+          if password == "fish":
+                  break
+print("OK")
